e043efdac6a0eb76b1a5f0a206fafa652069815e
Vinith69 <ramarock09@gmail.com>
2025-03-13 10:04:10 +0530

    nav desktop fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Nav from "@/components/Nav";
 import Image from "next/image";
+import "./homepage.css";
 
 export default function Home() {
   return (
     <>
-      <Nav />
+      <section
+        className="hero-section h-[75vh] flex flex-col items-center "
+        data-background="url(https://seasons.jdprojects.in/assets/img/hero/tu/banner.jpg)"
+      >
+        <Nav />
+        <div className="text-center self-center my-auto">
+          <h2 className="text-white text-5xl font-bold mb-1">Where Every Journey</h2>
+          <h3 className="text-white text-4xl font-bold">Become an Adventure</h3>
+
+          <div className="bg-white rounded-full w-[60vw] h-12 mt-8"></div>
+        </div>
+      </section>
       <main>Hi</main>
     </>
   );
