61aa8863f1e0a0929933ef79b901c95bf99d8814
Vinith69 <ramarock09@gmail.com>
2025-03-21 15:31:51 +0530

    popular section

--- components/Home/PopularTour.tsx
+++ components/Home/PopularTour.tsx
@@ -1,7 +1,8 @@
+import "./css/homepage.css";
 export default function PopularTour() {
   return (
     <section
-      className=" relative flex items-center justify-center h-[90vh] bg-cover bg-center bg-no-repeat"
+      className="relative flex items-center justify-center h-[90vh] bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url("https://seasons.jdprojects.in/assets/img/banner/banner.png")`,
       }}
@@ -11,15 +12,19 @@ export default function PopularTour() {
 
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4">
-        <p className="text-sm md:text-xl font-[caveat]  font-medium mb-1">Next Adventure Destination</p>
+        <p className="text-sm md:text-xl font-[caveat] font-medium mb-1">Next Adventure Destination</p>
         <h1 className="text-2xl md:text-5xl font-bold mb-4">
           Popular Travel Destinations
           <br className="hidden md:block" />
           Available Worldwide
         </h1>
+
         <button className="bg-[#f0e9ff] hover:bg-[#560ce3] text-[#560ce3] hover:text-white text-sm cursor-pointer font-semibold px-6 py-3 rounded-sm transition-all duration-300">
           BOOK YOUR TRIP NOW
         </button>
+
+        {/* Apply the custom class to get the stroke and partial-breakpoint font-size */}
+        <h2 className="tg-banner-2-big-title-h2">Explore the World</h2>
       </div>
     </section>
   );
